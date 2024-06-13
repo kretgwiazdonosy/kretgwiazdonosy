f915d1449d067b65d2bf31a9c667f09489a42cc5
kretgwiazdonosy <64355931+kretgwiazdonosy@users.noreply.github.com>
2024-06-13 11:49:02 +0200

    Update

---
+++ parzyste_nieparzyste.py
@@ -0,0 +1,10 @@
+# Plik: Parzyste_nieparzyste.py
+
+# Pobranie liczby od użytkownika
+liczba = int(input("Wprowadź liczbę całkowitą: "))
+    
+# Sprawdzenie parzystości
+if liczba % 2 == 0:
+    print(f"Liczba {liczba} jest parzysta.")
+else:
+    print(f"Liczba {liczba} jest nieparzysta.")

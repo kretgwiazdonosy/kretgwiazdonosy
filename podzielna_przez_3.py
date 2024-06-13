85c8a6ca778222453cfbf86eb57faae25d92c142
kretgwiazdonosy <64355931+kretgwiazdonosy@users.noreply.github.com>
2024-06-13 11:48:15 +0200

    Update

---
+++ podzielna_przez_3.py
@@ -0,0 +1,10 @@
+# Plik: Podzielna_przez_3.py
+
+# Pobranie liczby od użytkownika
+liczba = int(input("Wprowadź liczbę całkowitą: "))
+    
+# Sprawdzenie podzielności przez 3
+if liczba % 3 == 0:
+    print(f"Liczba {liczba} jest podzielna przez 3.")
+else:
+    print(f"Liczba {liczba} nie jest podzielna przez 3.")
